ca2453b4caa4f7f0dfddcfc2b54442acc5db61e2
Soulsby <jwsoulsby@gmail.com>
2016-01-05 18:17:46 -0500

    TestDeclarationVisitor Passing Tests
    testVisit() implemented.

--- Test/TestMisfitsPackage/TestDeclarationVisitor.java
+++ Test/TestMisfitsPackage/TestDeclarationVisitor.java
@@ -1,6 +1,14 @@
 package TestMisfitsPackage;
 
 import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.Scanner;
+
 import jdk.internal.org.objectweb.asm.Opcodes;
 
 import org.junit.Test;
@@ -20,8 +28,25 @@ public class TestDeclarationVisitor {
 		String[] interfaces=new String[2];
 		interfaces[0]="TestInterface1";
 		interfaces[1]="TestInterface2";
+		
+		String expected = "TestClass -> TestInterface1 [arrowhead=\"onormal\", style=\"dashed\"];"+System.lineSeparator()+
+				"TestClass -> TestInterface2 [arrowhead=\"onormal\", style=\"dashed\"];"+System.lineSeparator()+
+				"TestClass -> SuperTestClass [arrowhead=\"onormal\"];"+System.lineSeparator()+
+				"   TestClass ["+System.lineSeparator()+
+				"     shape=\"record\""+System.lineSeparator()+
+				"     label = \"{TestClass|";
+		
+		
+		// redirect stdout to save the result to a byte array
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
 		visitor.visit(version, access, name, signature, superName, interfaces);
-		fail("Not yet implemented");
+		
+		String actual = new String(out.toByteArray());
+		
+		assertEquals(expected, actual);
+		
 	}
 	
 }
