0b0a43900a80cef373b5d27833647ee13e7e00a5
cookmn <cookmn@rose-hulman.edu>
2016-01-07 23:32:57 -0500

    Wrote tests for different branches
    for public, private and protected

--- Test/TestMisfitsPackage/TestMethodVisitor.java
+++ Test/TestMisfitsPackage/TestMethodVisitor.java
@@ -17,18 +17,21 @@ public class TestMethodVisitor {
 
 	@Test
 	public void testVisitMethod() {
+		
+		String signature="Signature"; // these are not yet used in implementation
+		String[] exceptions = new String[2]; //same
+		exceptions[0]="Exception 1"; //same
+		exceptions[1]="Exception 2"; //same
+		
 		DesignParser.setFirstMethod(true);
 		
-		int access =0;
+		int access = Opcodes.ACC_PUBLIC;
 		String name = "testMethod";
 		String desc = Type.getMethodDescriptor(Type.getType(int.class),
                 Type.getType(double.class));
-		String signature="Signature";
-		String[] exceptions = new String[2];
-		exceptions[0]="Exception 1";
-		exceptions[1]="Exception 2";
 		
-		String expected = "|testMethod([double]) : int\\l ";
+		
+		String expected = "|+testMethod([double]) : int\\l ";
 
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(out));
@@ -39,5 +42,48 @@ public class TestMethodVisitor {
 		String actual = new String(out.toByteArray());
 
 		assertEquals(expected, actual);
+		
+		//I am now testing the access branch
+		DesignParser.setFirstMethod(true);
+		access = Opcodes.ACC_PRIVATE;
+		name = "testMethod";
+		desc = Type.getMethodDescriptor(Type.getType(int.class),
+                Type.getType(double.class));
+		
+		
+		expected = "|-testMethod([double]) : int\\l ";
+
+		out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		visitor = new ClassMethodVisitor(Opcodes.ASM5);
+		visitor.visitMethod(access, name, desc, signature, exceptions);
+
+		actual = new String(out.toByteArray());
+
+		assertEquals(expected, actual);
+		
+		//I am not testing the protected
+		//I am now testing the access branch
+		DesignParser.setFirstMethod(true);
+		access = Opcodes.ACC_PROTECTED;
+		name = "testMethod";
+		desc = Type.getMethodDescriptor(Type.getType(int.class),
+                Type.getType(double.class));
+		
+		
+		expected = "|#testMethod([double]) : int\\l ";
+
+		out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		visitor = new ClassMethodVisitor(Opcodes.ASM5);
+		visitor.visitMethod(access, name, desc, signature, exceptions);
+
+		actual = new String(out.toByteArray());
+
+		assertEquals(expected, actual);
 	}
+	
+	
 }
