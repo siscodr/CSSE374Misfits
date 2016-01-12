09f15ba92e03511e33b661e6606d3cc8bf164f2a
Donald Sisco <siscodr@rose-hulman.edu>
2016-01-12 14:41:22 -0500

    Changed ClassDeclarationVisitor
    The new ClassDeclarationVisitor only tells UMLArrows stuff and avoids
    doing anything else

--- src/MisfitsPackage/ClassDeclarationVisitor.java
+++ src/MisfitsPackage/ClassDeclarationVisitor.java
@@ -6,43 +6,20 @@ public class ClassDeclarationVisitor extends ClassVisitor {
 
 	public ClassDeclarationVisitor(int arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
-
-	// public ClassDeclarationVisitor(int arg0, ClassVisitor arg1) {
-	// super(arg0, arg1);
-	// }
 
 	public void visit(int version, int access, String name, String signature,
 			String superName, String[] interfaces) {
-		String newName = DesignParser.stripFunction(name);
-
+		
+		UMLArrows arrows = UMLArrows.getInstance();
+		// store interface arrows
 		for (int i = 0; i < interfaces.length; i++) {
-			String newInterface = DesignParser.stripFunction(interfaces[i]);
-			if (DesignParser.unwantedTypes(newInterface)) {
-				System.out.println(newName + " -> " + newInterface
-						+ " [arrowhead=\"onormal\", style=\"dashed\"];");
-			}
+			arrows.addInterface(interfaces[i]);
 		}
-		// Draw extend arrows here
+		// store extend arrow here
 		if (superName != null) {
-			String newSuper = DesignParser.stripFunction(superName);
-			if (DesignParser.unwantedTypes(newSuper)) {
-				System.out.println(newName + " -> " + newSuper
-						+ " [arrowhead=\"onormal\"];");
-			}
+			arrows.addSuper(superName);
 		}
-
-		// Class starts here
-		System.out.println("   " + newName + " [");
-		System.out.println("     shape=\"record\"");
-		System.out.print("     label = \"{" + newName + "|");
-
-		// System.out.print("Class: "+name);
-		// if(superName != null)
-		// System.out.print(" extends "+superName);
-		// if(interfaces.length > 0)
-		// System.out.println(" implements "+ Arrays.toString(interfaces));
 		super.visit(version, access, name, signature, superName, interfaces);
 	}
 }
