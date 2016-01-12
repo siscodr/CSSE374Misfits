7f029e6221365518ee2835a0d6be2e27226029b8
Donald Sisco <siscodr@rose-hulman.edu>
2016-01-12 14:42:23 -0500

    Changed ClassMethodVisitor
    ClassMethodVisitor only interacts with Singleton, UMLArrows

--- src/MisfitsPackage/ClassMethodVisitor.java
+++ src/MisfitsPackage/ClassMethodVisitor.java
@@ -20,16 +20,15 @@ public class ClassMethodVisitor extends ClassVisitor {
 
 	public MethodVisitor visitMethod(int access, String name, String desc,
 			String signature, String[] exceptions) {
+
 		MethodVisitor toDecorate = super.visitMethod(access, name, desc,
 				signature, exceptions);
+
 		MethodVisitor toReturn = new MyMethodVisitor(Opcodes.ASM5, toDecorate);
 
-		if (DesignParser.getFirstMethod()) {
-			System.out.print("|");
-			DesignParser.setFirstMethod(false);
-		}
-
-		String returnType = Type.getReturnType(desc).getClassName();
+		UMLArrows arrows = UMLArrows.getInstance();
+		
+		String rType = Type.getReturnType(desc).getClassName();
 
 		Type[] argTypes = Type.getArgumentTypes(desc);
 
@@ -38,44 +37,11 @@ public class ClassMethodVisitor extends ClassVisitor {
 			stypes.add(t.getClassName());
 		}
 
-		String symbol = "";
-		if ((access & Opcodes.ACC_PUBLIC) != 0) {
-			symbol = "+";
-		} else if ((access & Opcodes.ACC_PRIVATE) != 0) {
-			symbol = "-";
-		} else if ((access & Opcodes.ACC_PROTECTED) != 0) {
-			symbol = "#";
+		for (String types : stypes) {
+			arrows.addUse(types);
 		}
-		if (name.equals("<init>")) {
-			for (String types : stypes) {
-				String cleanType = DesignParser.stripFunction(types);
-				if (DesignParser.fields.contains(cleanType)
-						&& !DesignParser.unwantedTypes(cleanType)) {
-					DesignParser.fields.remove(cleanType);
-					DesignParser.takes.add(cleanType);
-				} else if (!DesignParser.uses.contains(cleanType)
-						&& DesignParser.unwantedTypes(cleanType)) {
-					DesignParser.uses.add(cleanType);
-				}
-			}
-		} else {
-			for (String types : stypes) {
-				String cleanType = DesignParser.stripFunction(types);
-				if (!DesignParser.uses.contains(cleanType)
-						&& !DesignParser.fields.contains(cleanType)
-						&& !DesignParser.takes.contains(cleanType)
-						&& DesignParser.unwantedTypes(cleanType)) {
-					DesignParser.uses.add(cleanType);
-				}
-			}
-		}
-
-		if (name.charAt(0) != '<') {
-
-			System.out.print(symbol + name + "(" + stypes.toString() + ") : "
-					+ returnType + "\\l ");
-		}
-
+		
+		arrows.addMethodToBuffer(access, name, stypes, rType);
 		return toReturn;
 	}
 }
