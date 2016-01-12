b6ff4cf6acc7d4c6f0bceda4b1f4b03260a3cecd
Donald Sisco <siscodr@rose-hulman.edu>
2016-01-12 15:06:45 -0500

    Commented ClassDeclarationVisitor
    JavaDoc + Comments

--- src/MisfitsPackage/ClassDeclarationVisitor.java
+++ src/MisfitsPackage/ClassDeclarationVisitor.java
@@ -4,22 +4,55 @@ import jdk.internal.org.objectweb.asm.ClassVisitor;
 
 public class ClassDeclarationVisitor extends ClassVisitor {
 
-	public ClassDeclarationVisitor(int arg0) {
-		super(arg0);
+	/**
+	 * Constructs a new ClassDeclarationVisitor
+	 * 
+	 * @param arg0
+	 *            the ASM API version implemented by this visitor. Must be one
+	 *            of Opcodes.ASM4.
+	 */
+	public ClassDeclarationVisitor(int api) {
+		super(api);
 	}
 
+	/**
+	 * This method decorates the ClassVisitor's visit function to add
+	 * functionality to pass information to UMLArrow inOrder to create a UML
+	 *
+	 * @param version
+	 *            the class version
+	 * @param access
+	 *            the class's access flags (see Opcodes). This parameter also
+	 *            indicates if the class is deprecated
+	 * @param name
+	 *            the internal name of the class
+	 * @param signature
+	 *            the signature of this class. May be null if the class is not a
+	 *            generic one, and does not extend or implement generic classes
+	 *            or interfaces.
+	 * @param superName
+	 *            The name of a class that this class extends from (Only should
+	 *            be null in case of Object)
+	 * @param interfaces
+	 *            An array of classes that the class inherits methods from
+	 */
 	public void visit(int version, int access, String name, String signature,
 			String superName, String[] interfaces) {
-		
-		UMLArrows arrows = UMLArrows.getInstance();
-		// store interface arrows
+
+		UMLArrows arrows = UMLArrows.getInstance(); // Get the Singleton
+													// Instance of UMLArrows
+
 		for (int i = 0; i < interfaces.length; i++) {
-			arrows.addInterface(interfaces[i]);
+			arrows.addInterface(interfaces[i]); // Store interface arrows into
+												// UMLArrows
 		}
-		// store extend arrow here
+
 		if (superName != null) {
-			arrows.addSuper(superName);
+			arrows.addSuper(superName); // Store super arrows (class extensions)
+										// into UMLArrows
 		}
+
+		// Does the decoratee's visit function
 		super.visit(version, access, name, signature, superName, interfaces);
 	}
 }
