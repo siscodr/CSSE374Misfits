8760201b7bcde9f69f26f0471fa275cac06ad25b
Donald Sisco <siscodr@rose-hulman.edu>
2016-01-12 20:08:54 -0500

    Commented MyMethodVisitor
    Added JavaDoc and Comments

--- src/MisfitsPackage/MyMethodVisitor.java
+++ src/MisfitsPackage/MyMethodVisitor.java
@@ -3,29 +3,81 @@ package MisfitsPackage;
 import jdk.internal.org.objectweb.asm.MethodVisitor;
 import jdk.internal.org.objectweb.asm.Type;
 
+/**
+ * MyMethodVisitor allows the functionality to pull types from inside of a
+ * method for the purpose of a UML
+ * 
+ * @author TheMisfits
+ */
 public class MyMethodVisitor extends MethodVisitor {
 
-	public MyMethodVisitor(int value) {
-		super(value);
+	/**
+	 * Constructs a new MyMethodVisitor.
+	 * 
+	 * @param api
+	 *            the ASM API version implemented by this visitor. Must be one
+	 *            of Opcodes.ASM4.
+	 */
+	public MyMethodVisitor(int api) {
+		super(api);
 	}
 
-	public MyMethodVisitor(int value, MethodVisitor methodVisitor) {
-		super(value, methodVisitor);
+	/**
+	 * Constructs a new MyMethodVisitor that decorates the old MethodVisitor.
+	 * 
+	 * @param api
+	 *            the ASM API version implemented by this visitor. Must be one
+	 *            of Opcodes.ASM4.
+	 * @param toDecorate
+	 *            A MethodVisitor for this class to Decorate
+	 */
+	public MyMethodVisitor(int api, MethodVisitor toDecorate) {
+		super(api, toDecorate);
 	}
 
+	/**
+	 * Decorates the visitTypeInsn method to get an use arrow for when a class
+	 * is constructed
+	 * 
+	 * @param opcode
+	 *            the opcode of the type instruction to be visited
+	 * @param val
+	 *            the operand of the instruction to be visited. This operand
+	 *            must be the internal name of an object or array class
+	 */
 	@Override
 	public void visitTypeInsn(int opCode, String val) {
 		super.visitTypeInsn(opCode, val);
+		// Creates an use arrow
 		UMLArrows.getInstance().addUse(val);
 	}
 
+	/**
+	 * Decorates the visitTypeInsn method to get an use arrow for when a class
+	 * is constructed
+	 * 
+	 * @param opcode
+	 *            the opcode of the type instruction to be visited.
+	 * @param owner
+	 *            the internal name of the method's owner class.
+	 * @param name
+	 *            the method's name.
+	 * @param desc
+	 *            the method's descriptor.
+	 * @param bool
+	 *            if the method's owner class is an interface.
+	 */
 	@Override
 	public void visitMethodInsn(int opcode, String owner, String name,
 			String desc, boolean bool) {
 		super.visitMethodInsn(opcode, owner, name, desc, bool);
+
 		String toClean = Type.getReturnType(desc).getClassName();
+
 		UMLArrows arrows = UMLArrows.getInstance();
+		// Adds first use arrow here
 		arrows.addUse(toClean);
+		// Adds second use arrow here
 		arrows.addUse(owner);
-		}
+	}
 }
