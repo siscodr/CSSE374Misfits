99c4c240795ef31ed60e1aa623ee4d3a4f289be1
Donald Sisco <siscodr@rose-hulman.edu>
2016-01-13 10:18:27 -0500

    Added testresetUMLArrows
    Included a value test too, since this is more important than calling
    resetUMLArrows on a new UMLArrows

--- Test/TestMisfitsPackage/UMLArrowsTest.java
+++ Test/TestMisfitsPackage/UMLArrowsTest.java
@@ -80,10 +80,10 @@ public class UMLArrowsTest {
 	public void testgetUses() throws NoSuchFieldException,
 			IllegalArgumentException, IllegalAccessException {
 		UMLArrows arrows = UMLArrows.getInstance();
-		Field fields = UMLArrows.class.getDeclaredField("uses");
-		fields.setAccessible(true);
+		Field uses = UMLArrows.class.getDeclaredField("uses");
+		uses.setAccessible(true);
 		ArrayList<String> usesv1 = new ArrayList<String>();
-		fields.set(arrows, usesv1);
+		uses.set(arrows, usesv1);
 		// Checking for same instance using ==
 		assertTrue(arrows.getUses() == usesv1);
 		ArrayList<String> usesv2 = new ArrayList<String>();
@@ -95,11 +95,11 @@ public class UMLArrowsTest {
 	public void testgetUsesValues() throws NoSuchFieldException,
 			IllegalArgumentException, IllegalAccessException {
 		UMLArrows arrows = UMLArrows.getInstance();
-		Field fields = UMLArrows.class.getDeclaredField("uses");
-		fields.setAccessible(true);
+		Field uses = UMLArrows.class.getDeclaredField("uses");
+		uses.setAccessible(true);
 		ArrayList<String> usesv1 = new ArrayList<String>(Arrays.asList(
 				"String", "cóol", "Understood"));
-		fields.set(arrows, usesv1);
+		uses.set(arrows, usesv1);
 		// Checking for same instance using ==
 		assertEquals(arrows.getUses(), usesv1);
 		ArrayList<String> usesv2 = new ArrayList<String>();
@@ -111,10 +111,10 @@ public class UMLArrowsTest {
 	public void testgetInterfaces() throws NoSuchFieldException,
 			IllegalArgumentException, IllegalAccessException {
 		UMLArrows arrows = UMLArrows.getInstance();
-		Field fields = UMLArrows.class.getDeclaredField("interfaces");
-		fields.setAccessible(true);
+		Field interfaces = UMLArrows.class.getDeclaredField("interfaces");
+		interfaces.setAccessible(true);
 		ArrayList<String> interfacesv1 = new ArrayList<String>();
-		fields.set(arrows, interfacesv1);
+		interfaces.set(arrows, interfacesv1);
 		// Checking for same instance using ==
 		assertTrue(arrows.getInterfaces() == interfacesv1);
 		ArrayList<String> interfacesv2 = new ArrayList<String>();
@@ -126,11 +126,11 @@ public class UMLArrowsTest {
 	public void testgetInterfacesValues() throws NoSuchFieldException,
 			IllegalArgumentException, IllegalAccessException {
 		UMLArrows arrows = UMLArrows.getInstance();
-		Field fields = UMLArrows.class.getDeclaredField("interfaces");
-		fields.setAccessible(true);
+		Field interfaces = UMLArrows.class.getDeclaredField("interfaces");
+		interfaces.setAccessible(true);
 		ArrayList<String> interfacesv1 = new ArrayList<String>(Arrays.asList(
 				"String", "cóol", "Understood"));
-		fields.set(arrows, interfacesv1);
+		interfaces.set(arrows, interfacesv1);
 		// Checking for same instance using ==
 		assertEquals(arrows.getInterfaces(), interfacesv1);
 		ArrayList<String> interfacesv2 = new ArrayList<String>();
@@ -142,10 +142,10 @@ public class UMLArrowsTest {
 	public void testgetSupers() throws NoSuchFieldException,
 			IllegalArgumentException, IllegalAccessException {
 		UMLArrows arrows = UMLArrows.getInstance();
-		Field fields = UMLArrows.class.getDeclaredField("supers");
-		fields.setAccessible(true);
+		Field supers = UMLArrows.class.getDeclaredField("supers");
+		supers.setAccessible(true);
 		String supersv1 = "";
-		fields.set(arrows, supersv1);
+		supers.set(arrows, supersv1);
 		// Checking for same instance using ==
 		assertTrue(arrows.getSupers() == supersv1);
 	}
@@ -154,10 +154,10 @@ public class UMLArrowsTest {
 	public void testgetSupersValues() throws NoSuchFieldException,
 			IllegalArgumentException, IllegalAccessException {
 		UMLArrows arrows = UMLArrows.getInstance();
-		Field fields = UMLArrows.class.getDeclaredField("supers");
-		fields.setAccessible(true);
+		Field supers = UMLArrows.class.getDeclaredField("supers");
+		supers.setAccessible(true);
 		String supersv1 = "Understood";
-		fields.set(arrows, supersv1);
+		supers.set(arrows, supersv1);
 		// Checking for same instance using ==
 		assertEquals(arrows.getSupers(), supersv1);
 		String supersv2 = "Hello_World";
@@ -169,10 +169,10 @@ public class UMLArrowsTest {
 	public void testgetWhiteList() throws NoSuchFieldException,
 			IllegalArgumentException, IllegalAccessException {
 		UMLArrows arrows = UMLArrows.getInstance();
-		Field fields = UMLArrows.class.getDeclaredField("whitelist");
-		fields.setAccessible(true);
+		Field whitelist = UMLArrows.class.getDeclaredField("whitelist");
+		whitelist.setAccessible(true);
 		ArrayList<String> whitelistv1 = new ArrayList<String>();
-		fields.set(arrows, whitelistv1);
+		whitelist.set(arrows, whitelistv1);
 		// Checking for same instance using ==
 		assertTrue(arrows.getWhitelist() == whitelistv1);
 		ArrayList<String> whitelistv2 = new ArrayList<String>();
@@ -184,15 +184,81 @@ public class UMLArrowsTest {
 	public void testgetWhitelistValues() throws NoSuchFieldException,
 			IllegalArgumentException, IllegalAccessException {
 		UMLArrows arrows = UMLArrows.getInstance();
-		Field fields = UMLArrows.class.getDeclaredField("whitelist");
-		fields.setAccessible(true);
+		Field whitelist = UMLArrows.class.getDeclaredField("whitelist");
+		whitelist.setAccessible(true);
 		ArrayList<String> whitelistv1 = new ArrayList<String>(Arrays.asList(
 				"String", "cóol", "Understood"));
-		fields.set(arrows, whitelistv1);
+		whitelist.set(arrows, whitelistv1);
 		// Checking for same instance using ==
 		assertEquals(arrows.getWhitelist(), whitelistv1);
 		ArrayList<String> whitelistv2 = new ArrayList<String>();
 		// Checking for different instance using !=
 		assertTrue(!arrows.getWhitelist().equals(whitelistv2));
+	}
+
+	@Test
+	public void testresetUMLArrows() throws NoSuchFieldException,
+			SecurityException, IllegalArgumentException, IllegalAccessException {
+		UMLArrows arrows = UMLArrows.getInstance();
+		arrows.resetUMLArrows();
+		Field whitelist = UMLArrows.class.getDeclaredField("whitelist");
+		whitelist.setAccessible(true);
+		Field supers = UMLArrows.class.getDeclaredField("supers");
+		supers.setAccessible(true);
+		Field interfaces = UMLArrows.class.getDeclaredField("interfaces");
+		interfaces.setAccessible(true);
+		Field uses = UMLArrows.class.getDeclaredField("uses");
+		uses.setAccessible(true);
+		Field fields = UMLArrows.class.getDeclaredField("fields");
+		fields.setAccessible(true);
+		assertEquals(whitelist.get(arrows), new ArrayList<String>());
+		assertEquals(supers.get(arrows), "");
+		assertEquals(interfaces.get(arrows), new ArrayList<String>());
+		assertEquals(uses.get(arrows), new ArrayList<String>());
+		assertEquals(fields.get(arrows), new ArrayList<String>());
+	}
+
+	@Test
+	public void testresetUMLArrowsValues() throws NoSuchFieldException,
+			SecurityException, IllegalArgumentException, IllegalAccessException {
+		UMLArrows arrows = UMLArrows.getInstance();
+		// Get the fields
+
+		// //Whitelist is not reset in Reset Arrows, since it is consistent
+		// //between all class in UML
+		// Field whitelist = UMLArrows.class.getDeclaredField("whitelist");
+		// whitelist.setAccessible(true);
+		Field supers = UMLArrows.class.getDeclaredField("supers");
+		supers.setAccessible(true);
+		Field interfaces = UMLArrows.class.getDeclaredField("interfaces");
+		interfaces.setAccessible(true);
+		Field uses = UMLArrows.class.getDeclaredField("uses");
+		uses.setAccessible(true);
+		Field fields = UMLArrows.class.getDeclaredField("fields");
+		fields.setAccessible(true);
+		// Set the values
+		// whitelist.set(arrows,new ArrayList<String>(Arrays.asList(
+		// "String", "cóol", "Understood")));
+		supers.set(arrows, "This is a cool String");
+		interfaces.set(
+				arrows,
+				new ArrayList<String>(Arrays.asList("Looks interesting",
+						"cóol", "Understood")));
+		uses.set(
+				arrows,
+				new ArrayList<String>(Arrays.asList("String", "cóol",
+						"Testing123")));
+		fields.set(
+				arrows,
+				new ArrayList<String>(Arrays.asList("Helllllo", "cóol",
+						"Mic Check?")));
+		// Reset
+		arrows.resetUMLArrows();
+		// Test
+		// assertEquals(whitelist.get(arrows), new ArrayList<String>());
+		assertEquals(supers.get(arrows), "");
+		assertEquals(interfaces.get(arrows), new ArrayList<String>());
+		assertEquals(uses.get(arrows), new ArrayList<String>());
+		assertEquals(fields.get(arrows), new ArrayList<String>());
 	}
 }
