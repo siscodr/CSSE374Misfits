ff6c854ba04f9e51c7834445c4f8e68152389035
Soulsby <jwsoulsby@gmail.com>
2016-01-14 16:21:20 -0500

    Created SDCreatorMain

---
+++ src/MisfitsPackage/SDCreatorMain.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package MisfitsPackage;
+
+import java.io.IOException;
+
+/**
+ * @author soulsbjw
+ *
+ */
+public class SDCreatorMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException{
+		DesignParser.makeSD(args); // Runs the program
+	}
+
+}
