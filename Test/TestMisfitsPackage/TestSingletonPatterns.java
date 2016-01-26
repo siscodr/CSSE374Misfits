cb1fa870c798ad246a3cd29734248988fcd4818d
siscodr <siscodr@rose-hulman.edu>
2016-01-26 10:25:07 -0500

    Setup Tests so they don't print to Console

--- Test/TestMisfitsPackage/TestSingletonPatterns.java
+++ Test/TestMisfitsPackage/TestSingletonPatterns.java
@@ -2,18 +2,40 @@ package TestMisfitsPackage;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import MisfitsPackage.DesignParser;
 import MisfitsPackage.PatternDetector;
 import MisfitsPackage.SingletonDetector;
 import MisfitsPackage.UMLArrows;
+import MisfitsPackage.UMLCreatorMain;
+
 
 public class TestSingletonPatterns {
+	
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+	@Before
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+		System.setErr(new PrintStream(errContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+		System.setOut(null);
+		System.setErr(null);
+	}
+	
 	@Test
 	public void testRuntimeForSingleton() throws IOException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException{
 		UMLArrows arrows = UMLArrows.getInstance();
