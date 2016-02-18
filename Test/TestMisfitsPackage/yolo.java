04fa173c1f614745640c8162b14a668af111a5bc
cookmn <cookmn@rose-hulman.edu>
2016-02-18 15:22:43 -0500

    Can execute graphviz from java!!!

--- Test/TestMisfitsPackage/yolo.java
+++ Test/TestMisfitsPackage/yolo.java
@@ -1,16 +1,23 @@
 package TestMisfitsPackage;
 
-import java.io.IOException;
-
 import org.junit.Test;
 
 public class yolo {
 
 	@Test
-	public void test() throws IOException {
-		Runtime runtime = Runtime.getRuntime();
-		Process process = runtime
-				.exec("C:/Program Files (x86)/Graphviz2.38/bin/gvedit.exe C:/Users/siscodr/Documents/GitHub/CSSE374Misfits/drawableGraph.gv");
+	public void test() {
+		try {
+			String[] com = new String[]{"C:\\Program Files (x86)\\Graphviz2.38\\bin\\dot.exe","-Tpng", "-o", "morgansCoolGraph.png", "drawableGraph.gv"};
+			//String temp = "\"C:\\Program Files (x86)\\Graphviz2.38\\bin\\dot.exe\" -Tpng -o morgansCoolGraph.png drawableGraph.gv";
+			ProcessBuilder pb = new ProcessBuilder(com);
+			//pb.redirectOutput(new File("morgansCoolGraph.png"));
+			Process p = pb.start();
+			p.waitFor();
+			System.out.println(p.exitValue());
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		// "C:\Program Files (x86)\Graphviz2.38\bin\dot" -Tpng drawableGraph.gv
+		// > graph.png
 	}
-
 }
