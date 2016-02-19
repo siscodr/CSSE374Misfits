f0dfbd42322b39719a0b3a87a67cec19f77c149d
Donald Sisco <siscodr@rose-hulman.edu>
2016-02-18 23:12:11 -0500

    Implemented ClassFinder to Main

--- src/UMLClasses/UMLCreatorMain.java
+++ src/UMLClasses/UMLCreatorMain.java
@@ -2,6 +2,7 @@ package UMLClasses;
 
 import java.io.IOException;
 
+import MisfitsPackage.ClassFinder;
 import MisfitsPackage.DesignParser;
 
 public class UMLCreatorMain {
@@ -15,10 +16,23 @@ public class UMLCreatorMain {
 	 *             Exception where string doesn't link to a class
 	 */
 	public static void main(String[] args) throws IOException {
-		UMLArrows.getInstance().addDetector(new SingletonDetector("orange", "purple"));
-		UMLArrows.getInstance().addDetector(new DecoratorDetector("green", "blue"));
-		UMLArrows.getInstance().addDetector(new CompositeDetector("deepskyblue1", "coral"));
-		UMLArrows.getInstance().addDetector(new AdapterDetector("deeppink", "grey40"));
-		DesignParser.makeUML(args); // Runs the program
+		UMLArrows.getInstance().addDetector(
+				new SingletonDetector("orange", "purple"));
+		UMLArrows.getInstance().addDetector(
+				new DecoratorDetector("green", "blue"));
+		UMLArrows.getInstance().addDetector(
+				new CompositeDetector("deepskyblue1", "coral"));
+		UMLArrows.getInstance().addDetector(
+				new AdapterDetector("deeppink", "grey40"));
+		if (args.length == 1) {
+			DesignParser.makeUML(ClassFinder.getClasses(args[0])); // Runs the
+																	// program
+		}
+		else if(args.length == 0) {
+			System.out.println("Not enough Arguements");
+		}
+		else {
+			DesignParser.makeUML(args);
+		}
 	}
 }
